513fd105836cf6b1e6191c0234acb19456db6bda
vibhoregoel915 <96377456+vibhoregoel915@users.noreply.github.com>
2022-01-01 13:18:52 +0530

    Add files via upload

---
+++ bootEMS/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `role_table` (`role_id`, `role_name`) VALUES ('1', 'ADMIN');
+INSERT INTO `user_table` (`user_id`, `username`, `password`) VALUES ('1', 'vibhu', '$2a$12$MHKA8n90UE8wdFtKlqrdbuFAeeaFAFZ8V5i9BUbomuYnlRTXg0v8i');
+INSERT INTO `user_roles` VALUES ('1', '1');
